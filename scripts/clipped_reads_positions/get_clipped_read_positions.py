ffb35fada2b6406ed1021a58db241f592d0d7dd8
lsantuari <5532a84eab8e88bffdaf4ee2cb98a125b9a8c753>
2018-02-28 17:42:00 +0100

    added sets with query_name of clipped reads by read1 or read2

--- scripts/clipped_reads_positions/get_clipped_read_positions.py
+++ scripts/clipped_reads_positions/get_clipped_read_positions.py
@@ -16,6 +16,14 @@ def get_clipped_read_positions(ibam, chrName, outFile):
     bamfile = pysam.AlignmentFile(ibam, "rb")
 
     clipped_pos = []
+    clipped_read_1 = set()
+    clipped_read_2 = set()
+
+    def add_clipped_read(read):
+        if read.is_read1:
+            clipped_read_1.add(read.query_name)
+        if read.is_read2:
+            clipped_read_2.add(read.query_name)
 
     for read in bamfile.fetch(chrName):
         if (not read.is_unmapped) and (not read.mate_is_unmapped):
@@ -23,9 +31,11 @@ def get_clipped_read_positions(ibam, chrName, outFile):
             # assert read.cigartuples[0][0] in [4, 5] or read.cigartuples[-1][0] in [4, 5]
             if len(read.get_reference_positions()) > 0:
                 if fun.is_left_clipped(read):
+                    add_clipped_read(read)
                     cpos = read.get_reference_positions()[0] + 1
                     clipped_pos.append(cpos)
                 if fun.is_right_clipped(read):
+                    add_clipped_read(read)
                     cpos = read.get_reference_positions()[-1] + 1
                     clipped_pos.append(cpos)
     #clipped_pos = list(set(clipped_pos))
@@ -35,12 +45,12 @@ def get_clipped_read_positions(ibam, chrName, outFile):
 
     # cPickle data persistence
     with bz2.BZ2File(outFile, 'wb') as f:
-        pickle.dump(clipped_pos_cnt, f)
+        pickle.dump((clipped_pos_cnt, clipped_read_1, clipped_read_2), f)
 
 
 def main():
     wd = "/Users/lsantuari/Documents/Data/HPC/DeepSV/Artificial_data/SURVIVOR-master/Debug/"
-    inputBAM = wd + "reads_chr17_SURV10kDEL_INS_Germline2_Somatic1_mapped/GS/mapping/" + "GS_dedup.subsampledto30x.bam"
+    inputBAM = wd + "reads_chr17_SURV10kDEL_INS_Germline2_Somatic1_mapped/GS/mapping/" + "GS_dedup.subsampledto30x.cr.bam"
 
     parser = argparse.ArgumentParser(description='Get clipped reads positions')
     parser.add_argument('-b', '--bam', type=str,
