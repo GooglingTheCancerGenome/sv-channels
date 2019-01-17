67bef8cc093e8c560a26d2a4d70d4c400b930d99
lsantuari <5532a84eab8e88bffdaf4ee2cb98a125b9a8c753>
2019-01-17 22:17:25 +0100

    Fixes dictionary subsetting

--- scripts/genome_wide/channel_maker_real_germline.py
+++ scripts/genome_wide/channel_maker_real_germline.py
@@ -1554,8 +1554,11 @@ def channel_maker(ibam, chrList, sampleName, SVmode, trainingMode, outFile):
 
                 clipped_pos_keep = set(clipped_pos[sample_list[0]]) - set(clipped_pos[sample_list[1]])
 
-                clipped_pos_cnt[chrName] = {k: v for (k, v) in clipped_pos_cnt_per_sample[sample_list[0]]
-                                            if k in clipped_pos_keep}
+                # clipped_pos_cnt[chrName] = {k: v for (k, v) in clipped_pos_cnt_per_sample[sample_list[0]]
+                #                             if k in clipped_pos_keep}
+
+                clipped_pos_cnt[chrName] = {k: clipped_pos_cnt[chrName][k]
+                                            for k in clipped_pos_cnt[chrName].keys() & clipped_pos_keep}
 
                 # Count the number of clipped read positions with a certain minimum number of clipped reads
                 logging.info('Clipped read positions with support only in the Tumor:')
