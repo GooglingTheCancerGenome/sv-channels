62a7bceb3a05ff318cd556c28d681093f5844ccf
lsantuari <5532a84eab8e88bffdaf4ee2cb98a125b9a8c753>
2019-07-16 15:54:38 +0200

    Fixes SGE scripts

--- scripts/genome_wide/SGE_scripts/submit_label_germline.sh
+++ scripts/genome_wide/SGE_scripts/submit_label_germline.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+OUTPATH='/hpc/cog_bioinf/ridder/users/lsantuari/Processed/DeepSV/channel_data/'
 PRG="make_label_germline"
 
 for SAMPLE in "NA12878"; do
     for WINDOW in 200; do
 
-		OUTDIR=$SAMPLE
+		OUTDIR=$OUTPATH$SAMPLE
 		JOB_NAME=$SAMPLE"_"$WINDOW"_"${PRG}
-		qsub -v SAMPLEARG=${SAMPLE},WINDOWARG=${WINDOW} \
+		qsub -v SAMPLEARG=${SAMPLE},WINDOWARG=${WINDOW},OUTARG=${OUTDIR} \
 			-N $JOB_NAME -o $JOB_NAME".out" -e $JOB_NAME".err" ${PRG}.sge
 
 	done
