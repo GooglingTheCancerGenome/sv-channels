bc0894a1ddf665e6f02a8e32ce30872c80b3ac64
lsantuari <5532a84eab8e88bffdaf4ee2cb98a125b9a8c753>
2019-11-27 15:02:45 +0100

    Wrapper SGE scripts for tier1 genome scan

---
+++ scripts/model/tier1/SGE_scripts/submit_tier1_scan.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+MODEL="/hpc/cog_bioinf/ridder/users/lsantuari/Processed/DeepSV/channel_data/NA12878/cnn/train_NA12878_test_NA12878_cv1/models/results_model_2.hdf5"
+OUTPATH='/hpc/cog_bioinf/ridder/users/lsantuari/Processed/DeepSV/channel_data/'
+
+CHRARRAY=(`seq 1 22` 'X' 'Y')
+
+PRG='scan_genome_with_model'
+
+SAMPLE_ARRAY=('NA24385')
+
+for (( i=0; i<${#SAMPLE_ARRAY[@]}; i++)); do
+#for i in 0; do
+	SAMPLE=${SAMPLE_ARRAY[$i]}
+
+    # for CHROMOSOME in ${CHRARRAY[@]}; do
+    for CHROMOSOME in '1'; do
+
+        for WINDOW in 200; do
+
+		    	OUTDIR=$OUTPATH
+		    	JOB_NAME=SAMPLE"_win"$WINDOW"_"${PRG}
+		    	qsub -v SAMPLE_ARG=${SAMPLE},CHRARG=${CHROMOSOME},MODELARG=${MODEL},PRGARG=${PRG},OUTDIRARG=${OUTDIR},CHANNELDIRARG=${OUTDIR},WINDOWARG=${WINDOW} \
+				    -N $JOB_NAME -o $JOB_NAME".out" -e $JOB_NAME".err" tier1_chromosome_scan.sge
+
+        done
+    done
+done
