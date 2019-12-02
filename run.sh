56c524d946108fcb4a2726f2d74152a3b745937c
Arnold Kuzniar <a.kuzniar@esciencecenter.nl>
2019-12-02 14:32:54 +0100

    Update wrapper: enable runs per chrom.

--- run.sh
+++ run.sh
@@ -9,13 +9,17 @@ conda install --file environment.yaml
 DATA_DIR=../../data/test
 BAM=hmz-sv.bam
 CHANNELS=(clipped_read_pos clipped_reads split_reads)
-for c in ${CHANNELS[@]}; do
+CHROMS=(17)
+for chn in ${CHANNELS[@]}; do
   SAMPLEARG=$(basename $BAM .bam)
   export SAMPLEARG=$SAMPLE
   export BAMARG=$DATA_DIR/$BAM
-  export PRGARG=$c
+  export PRGARG=$chn
   export OUTARG=$DATA_DIR
-  ./make_channel.sh
+  for chr in ${CHROMS[@]}; do
+    export CHRARG=$chr
+    ./make_channel.sh
+  done
 done
 
 echo -e "\nLog files:"
