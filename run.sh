cf99141249487cc470b66fb8e1258dad0ffc0443
Arnold Kuzniar <a.kuzniar@esciencecenter.nl>
2019-12-02 14:06:28 +0100

    Fix DATA_DIR path.

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ git clone -b $BRANCH https://github.com/GooglingTheCancerGenome/CNN.git
 cd CNN/scripts/genome_wide
 conda env create -n cm -f environment.yaml && conda activate cm
 
-DATA_DIR=../../test/data
+DATA_DIR=../../data/test
 BAM=hmz-sv.bam
 CHANNELS=(clipped_read_pos clipped_reads split_reads)
 for c in ${CHANNELS[@]}; do
