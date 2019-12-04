583a659fa97ead3c0ec60ce8026677f7cc553082
Arnold Kuzniar <a.kuzniar@esciencecenter.nl>
2019-12-04 18:05:10 +0100

    Simplify wrapper.

--- run.sh
+++ run.sh
@@ -10,7 +10,6 @@ fi
 
 source ~/.profile
 cd $HOME/scripts/genome_wide
-conda env update --file environment.yaml
 printenv
 
 # set [env] variables
@@ -18,31 +17,23 @@ SCH=$1
 BAM=$2
 BASE_DIR=$(dirname $BAM)
 SAMPLE=$(basename $BAM .bam)
-CHANNELS=(clipped_read_pos clipped_reads split_reads)
-CHROMS=(17)
-
-export SAMPLEARG=$SAMPLE
-export BAMARG=$BAM
-export OUTARG=$BASE_DIR
+PRGS=(clipped_read_pos clipped_reads split_reads)
+#CHROMS=(17)
 
 xenon --version
 
 # create channels per chromosome
-for chn in ${CHANNELS[@]}; do
-  export PRGARG=$chn
-  for chr in ${CHROMS[@]}; do
-    export CHRARG=$chr
-    xenon -v scheduler $SCH --location local:// submit --name $SAMPLE_$chn \
-      --cores-per-task 1 --inherit-env --max-run-time 1 --working-directory . \
-      --stderr stderr-%j.log --stdout stdout-%j.log ./make_channel.sh
-  done
+for prg in ${PRGS[@]}; do
+  xenon -v scheduler $SCH --location local:// submit --name $SAMPLE_$prg \
+    --cores-per-task 1 --inherit-env --max-run-time 1 --working-directory . \
+    --stderr stderr-%j.log --stdout stdout-%j.log "python $prg.py --bam $BAM --out $prg.json.gz --outputpath ."
 done
 
+sleep 10
+
 # write stdout/stderr logs into terminal
-cd $HOME
 echo -e "\nLog files:"
-#ls
-for f in $(find -type f -name std*.log); do
+for f in *.log; do
   echo "### $f ###"
   cat $f
 done
@@ -50,4 +41,4 @@ done
 # list channel outfiles (*.json.gz)
 echo -e "\nOutput files:"
 #ls
-find -type f -name \*.json.gz | grep "." || exit 1
+find -type f -name *.json.gz | grep "." || exit 1
