1a470bf84b9cbf03bfb08eb705031ffc8040c1c2
lsantuari <5532a84eab8e88bffdaf4ee2cb98a125b9a8c753>
2020-01-20 11:17:32 +0100

    Adds R scripts for plotting channel importance, filtering SVs and plotting benchmark results

---
+++ scripts/R/sv_comparison_functions.R
@@ -0,0 +1,197 @@
+suppressPackageStartupMessages(require(StructuralVariantAnnotation))
+
+#SV type assignment based on
+# https://github.com/PapenfussLab/gridss/blob/7b1fedfed32af9e03ed5c6863d368a821a4c699f/example/simple-event-annotation.R#L9
+apply_svtype <- function(gr)
+{
+  gr$svtype <-
+    ifelse(
+      seqnames(gr) != seqnames(partner(gr)),
+      "BP",
+      ifelse(
+        gr$insLen >= abs(gr$svLen) * 0.7,
+        "INS",
+        ifelse(
+          strand(gr) == strand(partner(gr)),
+          "INV",
+          ifelse(xor(
+            start(gr) < start(partner(gr)), strand(gr) == "-"
+          ), "DEL",
+          "DUP")
+        )
+      )
+    )
+  gr
+}
+
+# Load the HG002_SVs_Tier1_v0.6.bed confidence regions
+load_confidence_regions <- function(sample)
+{
+  if (sample == 'NA24385')
+  {
+    bed.file <-
+      "/Users/lsantuari/Documents/Data/germline/NA24385/NIST_SVs_Integration_v0.6/HG002_SVs_Tier1_v0.6.bed"
+  } else{
+    bed.file <-
+      "/Users/lsantuari/Documents/Data/ENCODE/ENCFF001TDO.bed"
+  }
+  confidence_regions_gr <- rtracklayer::import(bed.file)
+  seqlevelsStyle(confidence_regions_gr) <- "NCBI"
+  confidence_regions_gr
+}
+
+# keep only SVs with both breakpoints overlapping confidence regions
+remove_blacklist <- function(gr, confidence_regions_gr, sample)
+{
+  if (sample %in% c('NA24385'))
+  {
+    gr[overlapsAny(gr, confidence_regions_gr) &
+         overlapsAny(partner(gr), confidence_regions_gr), ]
+  } else{
+    gr[!(
+      overlapsAny(gr, confidence_regions_gr) |
+        overlapsAny(partner(gr), confidence_regions_gr)
+    ), ]
+  }
+  
+}
+
+
+load_sv_caller_vcf <-
+  function(vcf_file,
+           confidence_regions_gr,
+           sample,
+           sv_caller)
+  {
+    # vcf_file <- truth_set_file[[sample]]
+    sv_callset_vcf <-
+      VariantAnnotation::readVcf(vcf_file)
+    
+    if (sv_caller == 'lumpy')
+    {
+      # Read evidence support as a proxy for QUAL
+      support <- unlist(info(sv_callset_vcf)$SU)
+      fixed(sv_callset_vcf)$QUAL <- support
+    } else if (sv_caller == 'delly')
+    {
+      # Split-read support plus Paired-end read support as a proxy for QUAL
+      sr_support <- info(sv_callset_vcf)$SR
+      sr_support[is.na(sr_support)] <- 0
+      fixed(sv_callset_vcf)$QUAL <-
+        sr_support + info(sv_callset_vcf)$PE
+    }
+    
+    bpgr <- breakpointRanges(sv_callset_vcf)
+    begr <- breakendRanges(sv_callset_vcf)
+    gr <- sort(c(bpgr, begr))
+    if (sv_caller %in% c('gridss', 'manta')) {
+      gr <- apply_svtype(gr)
+    }
+    # Select DEL
+    gr <- gr[which(gr$svtype == "DEL")]
+    # Remove NAs
+    # gr <- gr[!is.na(gr$svLen)]
+    # Select DEL >= 50 bp
+    gr <- gr[abs(gr$svLen) >= 50]
+    gr <- remove_blacklist(gr, confidence_regions_gr, sample)
+    gr
+    
+  }
+
+load_truth_set_vcf <-
+  function(vcf_file, confidence_regions_gr, sample)
+  {
+    # vcf_file <- truth_set_file[[sample]]
+    sv_callset_vcf <-
+      VariantAnnotation::readVcf(vcf_file)
+    bpgr <- breakpointRanges(sv_callset_vcf)
+    begr <- breakendRanges(sv_callset_vcf)
+    gr <- sort(c(bpgr, begr))
+    #print(length(gr))
+    gr <- gr[which(gr$svtype == "DEL")]
+    if (sample %in% c('CHM1', 'CHM13', 'CHM1_CHM13'))
+    {
+      gr <- gr[gr$svLen >= 50]
+      seqlevelsStyle(gr) <- 'NCBI'
+      gr <-
+        keepSeqlevels(gr, c(1:22, 'X', 'Y'), pruning.mode = "coarse")
+    } else if (sample %in% c('NA24385')) {
+      gr <- gr[gr$FILTER == 'PASS']
+      gr <- gr[gr$svLen <= (-50)]
+    } else{
+      gr <- gr[gr$svLen <= (-50)]
+    }
+    
+    #gr <- apply_svtype(gr)
+    #print(length(gr))
+    # gr <- gr[which(gr$svtype == "DEL")]
+    #print(length(gr))
+    gr <- remove_blacklist(gr, confidence_regions_gr, sample)
+    #print(length(remove_blacklist(gr, confidence_regions_gr, sample)))
+    gr
+  }
+
+load_bedpe <- function(bedpe_file,
+                       confidence_regions_gr,
+                       sample)
+{
+  # bedpe_file <- truth_set_file[['NA12878']]
+  bpgr <- pairs2breakpointgr(rtracklayer::import(bedpe_file))
+  gr <- sort(bpgr)
+  gr <- remove_blacklist(gr, confidence_regions_gr, sample)
+  gr
+}
+
+make_percent <- function(x){
+  round(x*100,digits = 1)
+}
+
+load_callsets <- function(sample)
+{
+  
+  gr[[sample]] <- list()
+  
+  sv_caller_list <- c('gridss', 'manta', 'lumpy', 'delly')
+  
+  # Load sv_callers results
+  for (sv_caller in sv_caller_list)
+    #for(sv_caller in c('manta'))
+  {
+    # sv_caller <- 'gridss'
+    print(paste('Loading', sv_caller))
+    if (test_sample %in% c('NA12878', 'NA24385'))
+    {
+      vcf_file <-
+        file.path(
+          '/Users/lsantuari/Documents/Data/germline/',
+          test_sample,
+          'SV/Filtered',
+          paste(sv_caller, '.vcf', sep = '')
+        )
+    } else if (test_sample == 'CHM1_CHM13')
+    {
+      vcf_file <-
+        file.path(
+          '/Users/lsantuari/Documents/Data/germline/CHM/SV',
+          test_sample,
+          'Filtered',
+          paste(sv_caller, '.vcf', sep = '')
+        )
+    }
+    
+    # CHM1_CHM13 mapped with bwa mem
+    #vcf_file <- file.path('/Users/lsantuari/Documents/Data/germline/CHM/SV',sample,'Filtered',paste(sv_caller,'.vcf',sep=''))
+    print(paste('Loading ', vcf_file, sep = ''))
+    gr[[test_sample]][[sv_caller]] <-
+      load_sv_caller_vcf(vcf_file, confidence_regions_gr, test_sample, sv_caller)
+  }
+  
+  
+  for (c in sv_caller_list)
+  {
+    print(paste(c, length(gr[[test_sample]][[c]]), 'SVs'))
+  }
+  
+  return(gr)
+         
+}
