2f28e4b5e7f756b2a5f32bcdf63bfb0c1d1a318d
lsantuari <25436836+lsantuari@users.noreply.github.com>
2020-03-24 11:04:50 +0100

    Added scripts to generate test data: fasta, 2bit and bigwig

---
+++ notebooks/sandbox/utils/create_test_fasta.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+GENOME=$1
+BED=$2
+BW=$3
+CHRSIZES=$4
+OUTDIR=$5
+
+conda activate sv-gen_utils
+
+[ ! -d $OUTDIR ] && mkdir -p $OUTDIR
+cd $OUTDIR
+
+bedtools getfasta -fi $GENOME -bed $BED -fo test.tmp.fasta
+awk '{if($1~/^>/){split($1,a,":"); print a[1]}else{print}}' test.tmp.fasta > test.fasta
+
+faToTwoBit test.fasta test.2bit
+
+python ./bigwig_from_bed.py --bigwig $BW \
+    --bigwigout test.bw \
+    --bed $BED \
+    --chromsizes $CHRSIZES
