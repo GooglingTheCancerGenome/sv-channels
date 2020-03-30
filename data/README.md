504b4ea6f221f2b0d93b1cbdcc681abd90bc94e9
lsantuari <25436836+lsantuari@users.noreply.github.com>
2020-03-30 11:42:27 +0200

    Add script to create test data #40

--- data/README.md
+++ data/README.md
@@ -2,12 +2,18 @@ To create the test data:
 
 `cd utils`
 
-download the hs37d5 reference genome:
+download the hs37d5 reference genome from:
+
 ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
+
+and extract it:
+
+`gunzip hs37d5.fa.gz`
+
  
 ```
 conda env create -n test-env -f environment.yaml
 conda activate test-env
 
-sh create_test_data.sh hs37d5.fa.gz test_data
+sh create_test_data.sh hs37d5.fa test_data
 ```
--- utils/create_test_data.sh
+++ utils/create_test_data.sh
@@ -1,30 +1,27 @@
 #!/usr/bin/env bash
 
 REF=$(realpath -s "$1")
-REFNAME=$(basename "$REF" .fa.gz)
-
-# extract the FASTA file:
-gunzip $REF
+REFNAME=$(basename "$REF" .fa)
 
 OUTDIR=$2
-[ ! -d $OUTDIR ] && mkdir -p $OUTDIR
-cd $OUTDIR
+[ ! -d ${OUTDIR} ] && mkdir -p ${OUTDIR}
+cd ${OUTDIR}
 
 # specify read length the GEM mappability track
 READ_LENGTH=150
 
 # prepare the GEM mappability track in BigWig format:
-sh ../mappability/run_gem.sh $REF $REF.fa $READ_LENGTH .
+sh ../mappability/run_gem.sh ${REFNAME} ${REF} ${READ_LENGTH} .
 
 # get chromosome sizes
-samtools faidx ref.fa
-cut -f1,2 $REF.fa > sizes.genome
+samtools faidx -o ${REFNAME}.fai ${REF}
+cut -f1,2 ${REFNAME}.fai > sizes.genome
 
-GENOME=$REF.fa
+GENOME=${REF}
 BED=../data/seqs.bed
-BW=$REF.bw
+BW=${REFNAME}.${READ_LENGTH}mer.bw
 CHRSIZES=sizes.genome
 OUTDIR=.
 
 # extract chromosome regions
-../create_test_fasta.sh $GENOME $BED $BW $CHRSIZES $OUTDIR
+sh ../create_test_fasta.sh ${GENOME} ${BED} ${BW} ${CHRSIZES} ${OUTDIR}
