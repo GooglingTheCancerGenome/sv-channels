396a6ad53c433d91e0ce84fff40acc6b4a85b574
lsantuari <25436836+lsantuari@users.noreply.github.com>
2020-04-16 16:01:17 +0200

    Fix TRA in SURVIVOR VCF #55

--- scripts/R/vcf2bedpe.R
+++ scripts/R/vcf2bedpe.R
@@ -2,54 +2,54 @@
 
 suppressPackageStartupMessages(require(StructuralVariantAnnotation))
 library(tools)
-library(argparser, quietly=TRUE)
+library(argparser, quietly = TRUE)
 
-script.name<-basename(sub(".*=", "", commandArgs()[4])) # script name
+script.name <- basename(sub(".*=", "", commandArgs()[4])) # script name
 
 # create a parser and add command-line arguments
 p <- arg_parser("Convert VCF output of Manta, DELLY, LUMPY or GRIDSS to BEDPE format.")
-p <- add_argument(p, "-i", help="Input in VCF", type="character")
-p <- add_argument(p, "-o", help="Output in BEDPE", type="character")
-p <- add_argument(p, "-p", help="SVTYPE=INS if insertion length >= SV length * p",
-  type="numeric", default=0.7)
-p <- add_argument(p, "-l", help="Minimum SV length to consider", type="int",
-  default=50)
+p <- add_argument(p, "-i", help = "Input in VCF", type = "character")
+p <- add_argument(p, "-o", help = "Output in BEDPE", type = "character")
+p <- add_argument(p, "-p", help = "SVTYPE=INS if insertion length >= SV length * p",
+type = "numeric", default = 0.7)
+p <- add_argument(p, "-l", help = "Minimum SV length to consider", type = "int",
+default = 50)
 
 # parse the command line arguments
 argv <- parse_args(p)
 if (is.na(argv$i))
 {
     print(p)
-    q(status=1)
+    q(status = 1)
 }
 
 if (is.na(argv$o))
 {
-  argv$o = paste(file_path_sans_ext(argv$i), "bedpe", sep='.')
+    argv$o = paste(file_path_sans_ext(argv$i), "bedpe", sep = '.')
 }
 
 # SV types assigned according to
 # https://github.com/PapenfussLab/gridss/blob/7b1fedfed32af9e03ed5c6863d368a821a4c699f/example/simple-event-annotation.R#L9
 apply_svtype <- function(gr, p_inslen)
 {
-  gr$svtype <-
+    gr$svtype <-
     ifelse(
-      seqnames(gr) != seqnames(partner(gr)),
-      "TRA",
-      ifelse(
-        gr$insLen >= abs(gr$svLen) * p_inslen,
-        "INS",
-        ifelse(
-          strand(gr) == strand(partner(gr)),
-          "INV",
-          ifelse(xor(
-            start(gr) < start(partner(gr)), strand(gr) == "-"
-          ), "DEL",
-          "DUP")
-        )
-      )
+    seqnames(gr) != seqnames(partner(gr)),
+    "TRA",
+    ifelse(
+    gr$insLen >= abs(gr$svLen) * p_inslen,
+    "INS",
+    ifelse(
+    strand(gr) == strand(partner(gr)),
+    "INV",
+    ifelse(xor(
+    start(gr) < start(partner(gr)), strand(gr) == "-"
+    ), "DEL",
+    "DUP")
     )
-  gr
+    )
+    )
+    gr
 }
 
 # t est if there is at least one argument: if not, return an error
@@ -63,11 +63,26 @@ apply_svtype <- function(gr, p_inslen)
 
 sv_callset_vcf <- VariantAnnotation::readVcf(argv$i)
 
-# handle SURVIVOR simSV SVTYPE <TRA> as TIGRA <CTX>
-if("TRA" %in% info(sv_callset_vcf)$SVTYPE & !("CT" %in% names(info(sv_callset_vcf))))
-{
-  info(sv_callset_vcf)$SVTYPE[info(sv_callset_vcf)$SVTYPE=="TRA"] <- "CTX"
-}
+# update info header
+info(header(sv_callset_vcf)) <- rbind(info(header(sv_callset_vcf)),
+    data.frame(Number = '4', Type = 'String', Description = 'DELLY CT'))
+# update info
+info(sv_callset_vcf) <- cbind(info(sv_callset_vcf),
+    data.frame(CT = factor(rep('3to5', nrow(info(sv_callset_vcf))),
+        levels = c('5to5', '3to3', '3to5', '5to3'))))
+# TRA
+idx <- which(info(sv_callset_vcf)$SVTYPE == 'TRA')
+info(sv_callset_vcf)$CT[idx[seq(1, length(idx), by = 2)]] <- '3to3'
+info(sv_callset_vcf)$CT[idx[seq(2, length(idx), by = 2)]] <- '5to5'
+
+# INV
+idx <- which(info(sv_callset_vcf)[['SVTYPE']] == 'INV')
+info(sv_callset_vcf)$CT[idx[seq(1, length(idx), by = 2)]] <- '3to5'
+info(sv_callset_vcf)$CT[idx[seq(2, length(idx), by = 2)]] <- '5to3'
+
+# other SVTYPEs
+idx <- which(! info(sv_callset_vcf)[['SVTYPE']] %in% c('INV', 'TRA'))
+info(sv_callset_vcf)$CT[idx] <- '3to5'
 
 # Not including breakends (unpaired breakpoints)
 # bpgr <- breakpointRanges(sv_callset_vcf)
@@ -77,7 +92,7 @@ if("TRA" %in% info(sv_callset_vcf)$SVTYPE & !("CT" %in% names(info(sv_callset_vc
 
 gr <- breakpointRanges(sv_callset_vcf)
 
-gr <- apply_svtype(gr, p_inslen=argv$p)
+gr <- apply_svtype(gr, p_inslen = argv$p)
 # select SVs >= 50 bp. svLen==NA for svtype=='BP'
 gr <- gr[abs(gr$svLen) >= argv$l | gr$svtype == 'TRA']
 
@@ -88,15 +103,15 @@ svtype_vec <- gr$svtype
 names(svtype_vec) <- names(gr)
 
 # typeof(svtype_vec) must be equal to typeof(bedpe_keys)
-bedpe_keys <- as.vector(bedpe[,7])
-bedpe_svtype <- cbind(bedpe[,1:6], svtype_vec[bedpe_keys])
+bedpe_keys <- as.vector(bedpe[, 7])
+bedpe_svtype <- cbind(bedpe[, 1 : 6], svtype_vec[bedpe_keys])
 
 # check that all SVs with svtype==BP have breakpoints on different chromosomes
-if(any(bedpe_svtype[,1]==bedpe_svtype[,4]&bedpe_svtype[,7]=='TRA'))
+if (any(bedpe_svtype[, 1] == bedpe_svtype[, 4] & bedpe_svtype[, 7] == 'TRA'))
 {
-  stop("Some SVs with svtype BP contain breakpoints that are both on the same chromosomes")
+    stop("Some SVs with svtype BP contain breakpoints that are both on the same chromosomes")
 }
 
 # write output in BEDPE
-write.table(file=argv$o, bedpe_svtype, quote=FALSE, row.names=FALSE,
-            col.names=FALSE, sep='\t')
+write.table(file = argv$o, bedpe_svtype, quote = FALSE, row.names = FALSE,
+col.names = FALSE, sep = '\t')
