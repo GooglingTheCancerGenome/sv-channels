fdc54e95006c52a7357c9fdedb310191ac39b154
lsantuari <25436836+lsantuari@users.noreply.github.com>
2020-08-27 15:49:34 +0200

    Add script to merge SV calls #64

--- scripts/R/benchmark_functions.R
+++ scripts/R/benchmark_functions.R
@@ -29,97 +29,112 @@ infer_svtype <- function(gr)
 # load a BED file and returns a GRanges object
 load_bed <- function(bed_file)
 {
-    bed_regions <- rtracklayer::import(bed_file)
-    # set NCBI seqlevels
-    seqlevelsStyle(bed_regions) <- "NCBI"
-    return(bed_regions)
+  bed_regions <- rtracklayer::import(bed_file)
+  # set NCBI seqlevels
+  seqlevelsStyle(bed_regions) <- "NCBI"
+  return(bed_regions)
 }
 
 # load a BEDPE file and returns a Pairs object
 load_bedpe <- function(bedpe_file, filter_regions)
 {
-    bedpe_gr <- pairs2breakpointgr(rtracklayer::import(bedpe_file))
-    bedpe_gr <- filter_regions(bedpe_gr, load_bed(filter_regions), mode = 'remove')
-    return(bedpe_gr)
+  bedpe_gr <- pairs2breakpointgr(rtracklayer::import(bedpe_file))
+  bedpe_gr <- filter_regions(bedpe_gr, load_bed(filter_regions), mode='remove')
+  return(bedpe_gr)
 }
 
 # Filter regions using a BED file either for inclusion (keep) or exclusion (remove)
 filter_regions <- function(regions_to_filter, ref_regions, mode='remove')
 {
-    print(length(regions_to_filter))
-    if (mode == 'keep')
-    {
-        result <- regions_to_filter[overlapsAny(regions_to_filter, ref_regions) &
-        overlapsAny(partner(regions_to_filter), ref_regions),]
-    } else if (mode == 'remove') {
-        result <- regions_to_filter[! (
-        overlapsAny(regions_to_filter, ref_regions) |
+  print(length(regions_to_filter))
+  if (mode == 'keep')
+  {
+    result <- regions_to_filter[overlapsAny(regions_to_filter, ref_regions) &
+                                  overlapsAny(partner(regions_to_filter), ref_regions), ]
+  } else if (mode == 'remove'){
+    result <- regions_to_filter[!(
+      overlapsAny(regions_to_filter, ref_regions) |
         overlapsAny(partner(regions_to_filter), ref_regions)
-        ),]
-    }
-    print(length(result))
-    return(result)
+    ), ]
+  }
+  print(length(result))
+  return(result)
 }
 
 # load a VCF file and returns a Pairs object
 load_vcf <- function(vcf_file, svtype, caller, filter_regions)
 {
-
-    # Load VCF file
-    vcf_gr <-
-      VariantAnnotation::readVcf(vcf_file)
-
-    # set NCBI seqlevels
-    seqlevelsStyle(vcf_gr) <- 'NCBI'
-
-    if(caller=='survivor')
+  message(paste('Loading ', svtype, 'calls for', caller))
+  # Load VCF file
+  vcf_gr <-
+    VariantAnnotation::readVcf(vcf_file)
+  
+  # set NCBI seqlevels
+  seqlevelsStyle(vcf_gr) <- 'NCBI'
+  
+  if(caller=='survivor')
+  {
+    # update info header
+    info(header(vcf_gr)) <- rbind(info(header(vcf_gr)),
+                                  data.frame(Number = '4', Type = 'String', Description = 'DELLY CT'))
+    # update info
+    info(vcf_gr) <- cbind(info(vcf_gr), 
+                          data.frame(CT=factor(rep('3to5',nrow(info(vcf_gr))), 
+                                               levels=c('5to5', '3to3', '3to5', '5to3'))))
+    # TRA
+    idx <- which(info(vcf_gr)$SVTYPE=='TRA')
+    info(vcf_gr)$CT[idx[seq(1, length(idx), by=2)]]  <- '3to3'
+    info(vcf_gr)$CT[idx[seq(2, length(idx), by=2)]]  <- '5to5'
+    
+    # INV
+    idx <- which(info(vcf_gr)[['SVTYPE']]=='INV')
+    info(vcf_gr)$CT[idx[seq(1, length(idx), by=2)]]  <- '3to5'
+    info(vcf_gr)$CT[idx[seq(2, length(idx), by=2)]]  <- '5to3'
+    
+    # other SVTYPEs
+    idx <- which(!info(vcf_gr)[['SVTYPE']]%in%c('INV','TRA'))
+    info(vcf_gr)$CT[idx]  <- '3to5'
+    
+    # SURVIVOR simSV assigns LowQual to all artificial SVs
+    vcf_gr <- vcf_gr[rowRanges(vcf_gr)$FILTER%in%c("LowQual")]
+    
+    if(svtype == 'INS')
     {
-        # SURVIVOR simSV assigns LowQual to all artificial SVs
-        vcf_gr <- vcf_gr[rowRanges(vcf_gr)$FILTER%in%c("LowQual")]
-
-        # handle SURVIVOR simSV SVTYPE <TRA> as TIGRA <CTX>
-        if("TRA" %in% info(vcf_gr)$SVTYPE & !("CT" %in% names(info(vcf_gr))))
-        {
-          info(vcf_gr)$SVTYPE[info(vcf_gr)$SVTYPE=="TRA"] <- "CTX"
-        }
-
-        if(svtype == 'INS')
-        {
-            info(vcf_gr)$END <- end(ranges(rowRanges(vcf_gr)))
-        }
-
-    }else{
-        # Keep only SVs that passed the filtering (PASS or .)
-        vcf_gr <- vcf_gr[rowRanges(vcf_gr)$FILTER%in%c("PASS",".")]
+      info(vcf_gr)$END <- end(ranges(rowRanges(vcf_gr)))
     }
-
-    if (caller == 'lumpy')
-    {
-      # Read evidence support as a proxy for QUAL
-      support <- unlist(info(vcf_gr)$SU)
-      fixed(vcf_gr)$QUAL <- support
-    } else if (caller == 'delly')
-    {
-      # Split-read support plus Paired-end read support as a proxy for QUAL
-      sr_support <- info(vcf_gr)$SR
-      sr_support[is.na(vcf_gr)] <- 0
-      fixed(vcf_gr)$QUAL <-
-        sr_support + info(vcf_gr)$PE
-    }
-
-    vcf_gr <- breakpointRanges(vcf_gr)
-    vcf_gr <- infer_svtype(vcf_gr)
-
-    # Select only one SV type
-    vcf_gr <- vcf_gr[which(vcf_gr$svtype == svtype)]
-
-    # Select SVs >= 50 bp
-    if(!svtype %in% c('TRA','INS'))
-      {
-        vcf_gr <- vcf_gr[abs(vcf_gr$svLen) >= 50]
-      }
-
-    #Filter regions
-    vcf_gr <- filter_regions(vcf_gr, load_bed(filter_regions), mode='remove')
-    return(vcf_gr)
+    
+  }else{
+    # Keep only SVs that passed the filtering (PASS or .)
+    vcf_gr <- vcf_gr[rowRanges(vcf_gr)$FILTER%in%c("PASS",".")]
+  }
+  
+  if (caller == 'lumpy')
+  {
+    # Read evidence support as a proxy for QUAL
+    support <- unlist(info(vcf_gr)$SU)
+    fixed(vcf_gr)$QUAL <- support
+  } else if (caller == 'delly')
+  {
+    # Split-read support plus Paired-end read support as a proxy for QUAL
+    sr_support <- info(vcf_gr)$SR
+    sr_support[is.na(vcf_gr)] <- 0
+    fixed(vcf_gr)$QUAL <-
+      sr_support + info(vcf_gr)$PE
+  }
+  
+  vcf_gr <- breakpointRanges(vcf_gr)
+  vcf_gr <- infer_svtype(vcf_gr)
+  
+  # Select only one SV type
+  vcf_gr <- vcf_gr[which(vcf_gr$svtype == svtype)]
+  message(length(vcf_gr))
+  # Select SVs >= 50 bp
+  if(!svtype %in% c('TRA','INS'))
+  {
+    vcf_gr <- vcf_gr[abs(vcf_gr$svLen) >= 50]
+  }
+  
+  #Filter regions
+  # vcf_gr <- filter_regions(vcf_gr, load_bed(filter_regions), mode='remove')
+  return(vcf_gr)
 }
---
+++ scripts/R/merge_sv_calls.R
@@ -0,0 +1,150 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(require(StructuralVariantAnnotation))
+suppressPackageStartupMessages(require(rtracklayer))
+suppressPackageStartupMessages(require(tools))
+suppressPackageStartupMessages(require(argparser))
+
+script.name <-
+  basename(sub(".*=", "", commandArgs()[4])) # script name
+
+script.dir <- dirname(commandArgs()[2])
+source(file.path(script.dir, 'R/benchmark_functions.R'))
+
+#source('~/Documents/Local_GitHub/sv-channels/scripts/R/benchmark_functions.R')
+
+# create a parser and add command-line arguments
+p <-
+  arg_parser("Convert sv-channels BEDPE predictions into VCF format.")
+p <-
+  add_argument(
+    p,
+    "-i",
+    help = paste("a path containing multiple BEDPE files (CV mode"),
+    type = "character"
+  )
+p <-
+  add_argument(p, "-f", help = "ENCODE blacklist", type = "character")
+p <-
+  add_argument(p, "-m", help = "mode: 'split_reads', 'gridss', 'manta', 'delly', 'lumpy'", type = "character")
+p <-
+  add_argument(p, "-o", help = "Output in BEDPE", type = "character")
+
+# parse the command line arguments
+argv <- parse_args(p)
+if (is.na(argv$i))
+{
+  message('Input path missing')
+  print(p)
+  q(status = 1)
+} else if (is.na(argv$o))
+{
+  message('Output file missing')
+  print(p)
+  q(status = 1)
+}
+
+input_path <- argv$i
+regions_for_filtering <- argv$f
+
+# For each svtype, collect all predictions in BEDPE format and merge the SVs using SVA
+sv_regions <- list()
+sv_types <- c('DEL', 'INS', 'INV', 'DUP', 'TRA')
+#for (svtype in )
+
+for (svtype in sv_types)
+{
+  if(file.exists(file.path(input_path, svtype)))
+  {
+  # svtype <- 'DEL'
+  print(svtype)
+  filenames <-
+    list.files(
+      path = file.path(input_path,
+                       svtype,
+                       mode),
+      pattern = "cnn_predictions.bedpe$",
+      recursive = TRUE,
+      full.names = TRUE
+    )
+  
+  print(filenames)
+  bedpe.file <-
+    file.path(input_path, paste(mode, '_', svtype, '.bedpe', sep = ''))
+  print(bedpe.file)
+  if (file.exists(bedpe.file)) {
+    file.remove(bedpe.file)
+  }
+  
+  for (f in filenames)
+  {
+    cmd <-
+      paste(
+        "awk '{if($1==$4){print $0\"\t*\t*\t\" $5-$2}else{print $0\"\t*\t*\t\"0}}'",
+        f,
+        '>>',
+        bedpe.file
+      )
+    system(cmd)
+  }
+  
+  print('Loading predictions...')
+  sv_regions[[svtype]] <-
+    load_bedpe(bedpe.file, regions_for_filtering)
+  
+  # rename NA. column of CNN into svLen
+  names(mcols(sv_regions[[svtype]]))[names(mcols(sv_regions[[svtype]])) ==
+                                       'NA.'] <- 'svLen'
+  
+  # Merge SVs
+  if (svtype != 'INS')
+  {
+    hits <- findBreakpointOverlaps(
+      sv_regions[[svtype]],
+      sv_regions[[svtype]],
+      # read pair based callers make imprecise calls.
+      # A margin around the call position is required when matching with the truth set
+      maxgap = 100,
+      # Since we added a maxgap, we also need to restrict the mismatch between the
+      # size of the events. We don't want to match a 100bp deletion with a
+      # 5bp duplication. This will happen if we have a 100bp margin but don't also
+      # require an approximate size match as well
+      sizemargin = 0.25,
+      ignore.strand = TRUE,
+      # We also don't want to match a 20bp deletion with a 20bp deletion 80bp away
+      # by restricting the margin based on the size of the event, we can make sure
+      # that simple events actually do overlap
+      restrictMarginToSizeMultiple = 0.5
+    )
+    data <- as.data.frame(hits)
+    getfirst <- function(x) {
+      x[1]
+    }
+    get_best_qual <-
+      function(x) {
+        x[which.max(sv_regions[[svtype]]$QUAL[x])]
+      }
+    aggdata <-
+      aggregate(data$queryHits,
+                by = list(data$subjectHits),
+                FUN = get_best_qual)
+    sv_regions[[svtype]] <- sv_regions[[svtype]][unique(aggdata$x)]
+    sv_regions[[svtype]] <-
+      sv_regions[[svtype]][sv_regions[[svtype]]$partner %in% names(sv_regions[[svtype]])]
+  }
+  print(length(sv_regions[[svtype]]))
+  }
+}
+
+# print(breakpointgr2pairs(sv_regions[[svtype]]))
+#callset <- unlist(GRangesList(sv_regions))
+
+# Export to BEDPE
+for (t in sv_types)
+{
+  if(file.exists(file.path(input_path, t)))
+  {
+  rtracklayer::export(breakpointgr2pairs(sv_regions[[t]]),
+                      con = paste("results_", t, ".bedpe", sep = ""))
+  }
+}
