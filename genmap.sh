6721ab986734262c210e96efc2a8faf4c0b1dc31
arnikz <a.kuzniar@esciencecenter.nl>
2020-11-23 13:11:28 +0100

    Add script to generate genome mappability track #60.

---
+++ genmap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -xe
+
+# check input arg(s)
+if [ $# -ne "4" ]; then
+  echo "Usage: $0 [FASTA] [BIGWIG] [k-mers] [# mismatches]"
+  exit 1
+fi
+
+FASTA=$1
+BIGWIG=$2
+KMERS=$3
+MAX_MISMATCH=$4
+BASE_DIR="$(dirname "$FASTA")"
+FAI="$FASTA.fai"
+INDEX="$BASE_DIR/genmap_index"
+MAP="$BASE_DIR/genmap_K${KMERS}_E${MAX_MISMATCH}"
+
+genmap index -F "$FASTA" -I "$INDEX"
+genmap map -K "$KMERS" -E "$MAX_MISMATCH" -I "$INDEX" -O "$MAP" -bg
+cut -f 1,2 "$FAI" > "$FAI.sizes"
+bedGraphToBigWig "$MAP.bedgraph" "$FAI.sizes" "$BIGWIG"
