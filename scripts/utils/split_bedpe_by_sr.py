8318662ea5d0320f415bf096f28bc6e4487e1379
lsantuari <25436836+lsantuari@users.noreply.github.com>
2021-01-20 16:33:00 +0100

    update script to work on BEDPE files

--- scripts/utils/split_bedpe_by_sr.py
+++ scripts/utils/split_bedpe_by_sr.py
@@ -11,11 +11,11 @@ import sys
 from collections import defaultdict
 from time import time
 
+sys.path.append('../genome_wide/')
+
 from functions import *
 from intervaltree import IntervalTree
 from label_windows import *
-
-sys.path.append('../genome_wide/')
 
 
 def parse_cl_args(in_args, caller):
@@ -28,17 +28,17 @@ def parse_cl_args(in_args, caller):
     if caller == 'test':
         mypath = os.path.join(pathout, 'test.bedpe')
     else:
-        mypath = os.path.join(pathout, caller + '.vcf')
+        mypath = os.path.join(pathout, caller + '.bedpe')
     description = "Split VCF by overlap on split read positions"
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("-i", "--input", type=str,
                         default=mypath,
                         help="VCF input file")
     parser.add_argument("-o", "--output", type=str,
-                        default=os.path.join(pathout, caller+'_sr.vcf'),
+                        default=os.path.join(pathout, caller+'_sr.bedpe'),
                         help="VCF output file")
     parser.add_argument("-n", "--output_nosr", type=str,
-                        default=os.path.join(pathout, caller+'_nosr.vcf'),
+                        default=os.path.join(pathout, caller+'_nosr.bedpe'),
                         help="VCF output file")
     parser.add_argument("-w", "--win", type=int,
                         default=200,
@@ -55,9 +55,9 @@ def parse_cl_args(in_args, caller):
     return args
 
 
-def get_srpos_from_bedpe(inbedpe):
+def get_srpos_from_bedpe(inbedpe, svtype):
     # Dictionary with chromosome keys to store SVs
-    srpos = []
+    srpos = set()
     with gzip.GzipFile(inbedpe, 'rb') as fin:
         for line in fin.readlines():
             columns = line.decode('utf8').rstrip().split("\t")
@@ -66,8 +66,11 @@ def get_srpos_from_bedpe(inbedpe):
                 columns[1]), int(columns[2])
             chrom2, pos2_start, pos2_end = str(columns[3]), int(
                 columns[4]), int(columns[5])
-            svtype = columns[6]
-            srpos.append((chrom1, pos1_start, chrom2, pos2_start))
+            type = columns[6]
+
+            if svtype == type:
+                srpos.add((chrom1, pos1_start, chrom2, pos2_start))
+    print('{} split read positions for {}'.format(len(srpos), svtype))
     return srpos
 
 
@@ -95,18 +98,23 @@ def search_tree_with_bedpe(cpos, trees_start, trees_end, win_hlen):
     last_t = time()
     for i, p in enumerate(cpos, start=1):
         chrom1, pos1, chrom2, pos2 = p
-        lookup_start.append(trees_start[chrom1]
-                            [pos1 - win_hlen:pos1 + win_hlen + 1])
+        lookup_start.append(trees_start[chrom1].envelop(
+                            pos1 - win_hlen, pos1 + win_hlen + 1))
         lookup_end.append(
-            trees_end[chrom2][pos2 - win_hlen:pos2 + win_hlen + 1])
+            trees_end[chrom2].envelop(
+                            pos2 - win_hlen, pos2 + win_hlen + 1))
     return lookup_start, lookup_end
 
 
 def main():
-    srpos = get_srpos_from_bedpe(os.path.join(pathout, 'split_reads.bedpe.gz'))
+
     for caller in ['manta', 'gridss', 'lumpy', 'delly', 'test']:
+
         print('Considering {}:'.format(caller))
         args = parse_cl_args(sys.argv[1:], caller)
+
+        srpos = get_srpos_from_bedpe(os.path.join(pathout, 'split_reads.bedpe.gz'), args.svtype)
+
         win_hlen = int(
             args.win / 2) if args.win % 2 == 0 else int((args.win + 1) / 2)
         filename, file_extension = os.path.splitext(args.input)
@@ -115,15 +123,22 @@ def main():
             sv_list = [i for i in sv_list if i[-1] == args.svtype]
         elif file_extension == '.bedpe':
             sv_list = read_bedpe(args.input, args.svtype)
+
         trees_start, trees_end = create_gtrees(sv_list)
         lookup_start, lookup_end = search_tree_with_bedpe(
             srpos, trees_start, trees_end, win_hlen)
         idx = []
+
+        sr_sv = set()
+        sr_nosv = set()
+
         for i, x in enumerate(zip(srpos, lookup_start, lookup_end), start=0):
             p, lu_start, lu_end = x
             l1 = len(lu_start)
             l2 = len(lu_end)
             if l1 > 0 and l2 > 0:
+
+                sr_sv.add(p)
                 lu_start_set = []
                 lu_end_set = []
                 for s in lu_start:
@@ -139,6 +154,8 @@ def main():
                 olap = set(lu_start_set) & set(lu_end_set)
                 if len(olap) > 0:
                     idx.extend(olap)
+            elif l1 == 0 and l2 == 0:
+                sr_nosv.add(p)
 
         print('list of indices: {}'.format(len(idx)))
         out = open(args.output, 'w')
@@ -159,18 +176,18 @@ def main():
                         nosr_lines += 1
                     j += 1
 
-        print('{} SVs => with SR:{}({}%); without:{}({}%)'.format(
+        print('{} SVs => with {} SR:{}({}%); without {} SR:{}({}%)'.format(
             sr_lines+nosr_lines,
+            len(sr_sv),
             sr_lines,
-            sr_lines/(sr_lines+nosr_lines)*100,
+            sr_lines/(sr_lines + nosr_lines) * 100,
+            len(sr_nosv),
             nosr_lines,
-            nosr_lines / (sr_lines + nosr_lines)*100,
+            nosr_lines / (sr_lines + nosr_lines) * 100,
         ))
-        out.close()
-        out_nosr.close()
 
 
 if __name__ == "__main__":
     global pathout
-    pathout = '/Users/lsantuari/Documents/Processed/SPIDER/split_reads/NA12878'
+    pathout = '/Users/lsantuari/Documents/Processed/SPIDER/split_reads/CHM1_CHM13/bedpe'
     main()
