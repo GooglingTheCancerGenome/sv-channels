257cb7f408d5fc4cdd4461132c1d7a89d88e4d7c
Brent Pedersen <bpederse@gmail.com>
2021-09-02 14:24:19 +0200

    working channels

--- svchannels/generate_channels.py
+++ svchannels/generate_channels.py
@@ -1,16 +1,17 @@
+import sys
+import os
+import re
+import time
+
 import numpy as np
 import numba
 import zarr
-
-import sys
-import os
 
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.insert(0, os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from svchannels.extract_signals import Event
-
 
 def find_signals_for_event(apos, bpos, signals2d, signals1d, expand=250):
     # note: only works on same chromosome as implemented.
@@ -36,11 +37,11 @@ def find_signals_for_event(apos, bpos, signals2d, signals1d, expand=250):
     # same chrom and with expected bounds on both ends.
     selection = (subset["b_pos"] >= (bpos - expand)) & (subset["b_pos"] <= (bpos + expand)) & (subset["a_chrom"] == subset["b_chrom"])
     result['shared'] = subset[selection]
-    result['left-only'] = subset[~selection][["a_chrom", "a_pos", "event"]].tolist()
+    result['left-only'] = subset[~selection][["a_chrom", "a_pos", "event"]] #.tolist()
 
     subsetb = signals2d[bidxl: bidxr]
     selectionb = (subsetb["a_pos"] >= apos - expand) & (subsetb["a_pos"] <= apos + expand) & (subsetb["a_chrom"] == subsetb["b_chrom"])
-    result['right-only'] =  subsetb[~selectionb][["a_chrom", "a_pos", "event"]].tolist()
+    result['right-only'] =  subsetb[~selectionb][["a_chrom", "a_pos", "event"]] #.tolist()
 
     
     # 1d search ...
@@ -53,14 +54,17 @@ def find_signals_for_event(apos, bpos, signals2d, signals1d, expand=250):
     bidxl = max(bidxl, idxl)
     bidxr = max(bidxr, bidxl)
 
-    result['left-only'].extend(signals1d[idxl:idxr])
-    result['right-only'].extend(signals1d[bidxl:bidxr])
-    result['left-only'] = np.array(result['left-only'], dtype=[('chrom', 'S8'), ('pos', np.int32), ('event', 'i2')])
-    result['right-only'] = np.array(result['right-only'], dtype=[('chrom', 'S8'), ('pos', np.int32), ('event', 'i2')])
+    result['left-only'].append(signals1d[idxl:idxr])
+    result['right-only'].append(signals1d[bidxl:bidxr])
+
+    #result['left-only'] = np.array(result['left-only'], dtype=[('chrom', 'S8'), ('pos', np.int32), ('event', 'i2')])
+    #result['right-only'] = np.array(result['right-only'], dtype=[('chrom', 'S8'), ('pos', np.int32), ('event', 'i2')])
+    result['left-only'].columns = ['chrom', 'pos', 'event'] 
+    result['right-only'].columns = ['chrom', 'pos', 'event'] 
 
     return result
 
-@numba.jit(nopython=True)
+#@numba.jit(nopython=True)
 def fill_arr(channels, events, posns, opos, offset):
 
     pmax = channels.shape[1]
@@ -74,7 +78,7 @@ def fill_arr(channels, events, posns, opos, offset):
 def generate_channels_for_event(apos, bpos, signals1d, signals2d, expand, gap, depths):
     r = find_signals_for_event(apos, bpos, signals2d, signals1d, expand=expand)
 
-    channels = np.zeros((max(Event), 4 * expand + gap), dtype=np.int32)
+    channels = np.zeros((max(Event) + 1, 4 * expand + gap), dtype=np.int32)
     # TODO: handle apos - expand < 0
     channels[0, 0:2*expand] = depths[apos - expand:apos + expand]
     channels[0, 2*expand+gap:] = depths[bpos - expand:bpos + expand]
@@ -82,8 +86,10 @@ def generate_channels_for_event(apos, bpos, signals1d, signals2d, expand, gap, d
     fill_arr(channels, np.asarray(r["shared"]["event"]), np.asarray(r["shared"]["a_pos"]), apos, expand)
     fill_arr(channels, np.asarray(r["shared"]["event"]), np.asarray(r["shared"]["b_pos"]), bpos, 3 * expand + gap)
 
-    fill_arr(channels, r["left-only"]["event"],  r["left-only"]["pos"], apos, expand)
-    fill_arr(channels, r["right-only"]["event"], r["right-only"]["pos"], bpos, 3 * expand + gap)
+    if len(r["left-only"]) > 0:
+        fill_arr(channels, np.asarray(r["left-only"]["event"]),  np.asarray(r["left-only"]["pos"]), apos, expand)
+    if len(r["right-only"]) > 0:
+        fill_arr(channels, np.asarray(r["right-only"]["event"]), np.asarray(r["right-only"]["pos"]), bpos, 3 * expand + gap)
 
     return channels
 
@@ -96,18 +102,10 @@ def xopen(filepath):
         else:
           return io.TextIOWrapper(open(filepath, 'rb'))
 
-def main(args=sys.argv[1:]):
-    import argparse
+def read_bins(directory):
     import glob
-    import re
-    p = argparse.ArgumentParser()
-    p.add_argument("directory", help="sample-specific directory created by svchannels extract")
-    p.add_argument("bedpe")
-
-    a = p.parse_args(args)
-
-    bins = glob.glob(a.directory + "/depths*.bin")
-    bin_patt = f"^{a.directory.rstrip('/')}\/depths\.(.+)\.bin$"
+    bins = glob.glob(directory + "/depths*.bin")
+    bin_patt = f"^{directory.rstrip('/')}\/depths\.(.+)\.bin$"
     bin_patt = re.compile(bin_patt)
     depths_by_chrom = {}
     for bin in bins:
@@ -116,18 +114,51 @@ def main(args=sys.argv[1:]):
         depths_by_chrom[chrom] = zarr.open(bin, mode='r')
       except AttributeError:
         raise ValueError(f"unable to find chrom in {bin}")
-   
+    return depths_by_chrom   
 
+def main(args=sys.argv[1:]):
+    import argparse
+    import pandas as pd
+    p = argparse.ArgumentParser()
+    p.add_argument("directory", help="sample-specific directory created by svchannels extract")
+    p.add_argument("bedpe")
+
+    a = p.parse_args(args)
+
+    depths_by_chrom = read_bins(a.directory)
+    e2d = pd.read_table(f"{a.directory}/sv-channels.events2d.txt.gz", compression="gzip",
+                        names=["a_chrom", "a_pos", "b_chrom", "b_pos", "event"],
+                        dtype={"a_chrom": str, "b_chrom": str})
+
+    e1d = pd.read_table(f"{a.directory}/sv-channels.soft_and_insertions.txt.gz", compression="gzip",
+                        names=["chrom", "pos", "event"],
+                        dtype={"chrom": str})
+    print(f"[svchannels] read {len(e2d)} 2D events and {len(e1d)} 1d events", file=sys.stderr)
+    t0 = time.time()
+    n = 0
+    expand = 250
+    gap = 10
 
     for line in xopen(a.bedpe):
-        print(line.strip())
-        break
+        toks = line.strip().split()
+        if toks[0] != toks[3]: continue
+        if int(toks[1]) < expand: continue
+        if int(toks[4]) < expand: continue
+        if int(toks[4]) < int(toks[1]):
+            toks[4], toks[1] = toks[1], toks[4]
+        n += 1
+        # here, sv_chan is shape (n_channels, 2 * expand + gap) can accumulate these and send to learner.
+        sv_chan = generate_channels_for_event(int(toks[1]), int(toks[4]), e1d, e2d, expand, gap, depths_by_chrom[toks[0]])
+ 
+    t = time.time() - t0
+    print(f"generated {n} channels in {t:.1f} seconds ({n/t:.0f} SVs/second)", file=sys.stderr)
     
 
     
 
 
 if __name__ == "__main__":
+    import pandas as pd
 
     outdir = "sv-channels"
     chrom = "1"
@@ -144,11 +175,13 @@ if __name__ == "__main__":
                   ('b_chrom', 'S8'), ('b_pos', np.int32),
                   ('event', 'i2')
                   ])
+    signals2d = pd.DataFrame(signals2d)
 
     signals1d = np.array([
         ("chr1", 11100, Event(3)),
         ("chr1", 11400, Event(3)),
         ], dtype=[('chrom', 'S8'), ('pos', np.int32), ('event', 'i2')])
+    signals1d = pd.DataFrame(signals1d)
 
     apos, bpos = (11100, 11400)
     print(f"querying {apos}, {bpos}")
@@ -164,6 +197,6 @@ if __name__ == "__main__":
     #    print(k)
     #    print(v.tolist())
 
-    print(generate_channels_for_event(apos, bpos, signals1d, signals2d, expand, gap, depths))
+    #print(generate_channels_for_event(apos, bpos, signals1d, signals2d, expand, gap, depths))
 
     main()
