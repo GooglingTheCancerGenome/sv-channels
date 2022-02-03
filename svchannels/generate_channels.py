f033958993737fe20818e42a188738e5779c43a1
Luca Santuari <25436836+lsantuari@users.noreply.github.com>
2022-02-03 10:33:03 +0100

    #95: add expand and gqp as input parameters

--- svchannels/generate_channels.py
+++ svchannels/generate_channels.py
@@ -128,6 +128,16 @@ def main(args=sys.argv[1:]):
     p = argparse.ArgumentParser()
     p.add_argument("directory", help="sample-specific directory created by svchannels extract")
     p.add_argument("bedpe")
+    parser.add_argument(
+        '--expand',
+        type=int,
+        default=250,
+        help="Specify width of single windows")
+    parser.add_argument(
+        '--gap',
+        type=int,
+        default=10,
+        help="Specify width of gap")
 
     a = p.parse_args(args)
 
@@ -142,8 +152,8 @@ def main(args=sys.argv[1:]):
     print(f"[svchannels] read {len(e2d)} 2D events and {len(e1d)} 1d events", file=sys.stderr)
     t0 = time.time()
     n = 0
-    expand = 250
-    gap = 10
+    expand = args.expand
+    gap = args.gap
 
     sv_chan = []
     # write the lines considered to file
