53e5e78699444bc1ed35b1997edaa1bc2e6516f9
Brent Pedersen <bpederse@gmail.com>
2022-05-31 11:50:58 +0200

    compress discordants by 8 so we can use smaller matricies

--- svchannels/generate_channels.py
+++ svchannels/generate_channels.py
@@ -5,12 +5,10 @@ import time
 from pathlib import Path
 from pyfaidx import Fasta
 
-
 import numpy as np
 np.set_printoptions(threshold=5000)
 
 import zarr
-
 from matplotlib import pyplot as plt
 
 PACKAGE_PARENT = '..'
@@ -19,8 +17,10 @@ sys.path.insert(0, os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from svchannels.extract_signals import Event, orphanable_events
 
+DISCORDANTS = [Event.DISCORDANT_PLUS_MINUS, Event.DISCORDANT_PLUS_PLUS, Event.DISCORDANT_MINUS_MINUS, Event.DISCORDANT_MINUS_PLUS]
 
-def find_signals_for_event(chrom, apos, bpos, signals2d_a, signals2d_b, signals1d, expand=250):
+
+def find_signals_for_event(chrom, apos, bpos, signals2d_a, signals2d_b, signals1d, disc_a, disc_b, expand=250):
     # signals2d_a is sorted by A psoition, so we can use search-sorted
     # we can use that to find things unique to a as well as things shared with a and b.
     # but we need signals2d_b to find things unique to b.
@@ -68,23 +68,54 @@ def find_signals_for_event(chrom, apos, bpos, signals2d_a, signals2d_b, signals1
 
     result['left-only'].extend(signals1d[idxl:idxr].to_dict(orient='records'))
     result['right-only'].extend(signals1d[bidxl:bidxr].to_dict(orient='records'))
+
+    # DISCORDANTS
+    # now get discordants. we must divide by 8. everthing else is the same.
+    apos >>= 3
+    bpos >>= 3
+    aidxl = np.searchsorted(disc_a["a_pos"], apos - expand, side="left")
+    aidxr = np.searchsorted(disc_a["a_pos"], apos + expand, side="right")
+    subset = disc_a[aidxl: aidxr]
+
+    selection = (subset["b_pos"] >= (bpos - expand)) & (subset["b_pos"] <= (bpos + expand)) & (
+            subset["a_chrom"] == chrom) & (subset["b_chrom"] == chrom)
+
+    result['shared-discordants'] = subset[selection].copy(deep=True)
+
+    lo = subset[~selection][["a_chrom", "a_pos", "event"]].copy(deep=True)
+    lo.columns = ("chrom", "pos", "event")
+    result['left-only-discordants'] = lo.to_dict(orient='records')
+
+    # discordants b-size
+    bidxl = np.searchsorted(disc_b["b_pos"], bpos - expand, side="left")
+    bidxr = np.searchsorted(disc_b["b_pos"], bpos + expand, side="right")
+
+    subsetb = disc_b[bidxl: bidxr]
+    selectionb = (subsetb["a_pos"] >= apos - expand) & (subsetb["a_pos"] <= apos + expand) & (
+            subsetb["a_chrom"] == chrom) & (subsetb["b_chrom"] == chrom)
+
+    ro = subsetb[~selectionb][["b_chrom", "b_pos", "event"]].copy(deep=True)
+    ro.columns = ("chrom", "pos", "event")
+    result['right-only-discordants'] = ro.to_dict(orient='records')
+
     return result
 
 
-def fill_arr(channels, events, posns, opos, offset):
+def fill_arr(channels, events, posns, opos, offset, shift=0):
     pmax = channels.shape[1]
 
     for i in range(len(events)):
-        pos = posns[i] - opos + offset
+        # shift is by 3 for discordants to get back into array coords
+        pos = posns[i] - (opos >> shift) + offset
         if pos < pmax and pos >= 0:
             channels[events[i], pos] += 1
 
-def fill_orphan_dicts(channels, dicts, opos, offset):
+def fill_orphan_dicts(channels, dicts, opos, offset, shift=0):
     pmax = channels.shape[1]
     m = max(Event) - 1
     for d in dicts:
 
-        pos = d['pos'] - opos + offset
+        pos = d['pos'] - (opos >> shift) + offset
         if pos < pmax and pos >= 0:
 
             if Event(d['event']) in orphanable_events:
@@ -95,8 +126,10 @@ def fill_orphan_dicts(channels, dicts, opos, offset):
 ONE_HOT = [np.array(l, dtype='c') for l in "ACTGN"]
 N_onehot = len(ONE_HOT) # ACTGN
 
-def generate_channels_for_event(chrom, apos, bpos, signals1d, signals2d_a, signals2d_b, expand, gap, depths, fasta):
-    r = find_signals_for_event(chrom, apos, bpos, signals2d_a, signals2d_b, signals1d, expand=expand)
+def generate_channels_for_event(chrom, apos, bpos, signals1d, signals2d_a, signals2d_b,
+                                edisc_a, edisc_b,
+                                expand, gap, depths, fasta):
+    r = find_signals_for_event(chrom, apos, bpos, signals2d_a, signals2d_b, signals1d, edisc_a, edisc_b, expand=expand)
 
     ChannelShape = (max(Event) + 1 + len(orphanable_events) + N_onehot, 4 * expand + gap)
     channels = np.zeros(ChannelShape, dtype=np.int32)
@@ -110,12 +143,18 @@ def generate_channels_for_event(chrom, apos, bpos, signals1d, signals2d_a, signa
 
     fill_arr(channels, np.asarray(r["shared"]["event"]), np.asarray(r["shared"]["a_pos"]), apos, expand)
     fill_arr(channels, np.asarray(r["shared"]["event"]), np.asarray(r["shared"]["b_pos"]), bpos, 3 * expand + gap)
+    fill_arr(channels, np.asarray(r["shared-discordants"]["event"]), np.asarray(r["shared-discordants"]["a_pos"]), apos, expand, shift=3)
+    fill_arr(channels, np.asarray(r["shared-discordants"]["event"]), np.asarray(r["shared-discordants"]["b_pos"]), bpos, 3 * expand + gap, shift=3)
 
     if len(r["left-only"]) > 0:
         fill_orphan_dicts(channels, r['left-only'], apos, expand)
+    if len(r["left-only-discordants"]) > 0:
+        fill_orphan_dicts(channels, r['left-only-discordants'], apos, expand, shift=3)
+
     if len(r["right-only"]) > 0:
-        # TODO this is broken from find_signals (not getting because not sorted on b
         fill_orphan_dicts(channels, r['right-only'], bpos, 3 * expand + gap)
+    if len(r["right-only-discordants"]) > 0:
+        fill_orphan_dicts(channels, r['right-only-discordants'], bpos, 3 * expand + gap, shift=3)
 
     """
     for i, row in enumerate(channels):
@@ -187,12 +226,12 @@ def main(args=sys.argv[1:]):
     p.add_argument(
         '--expand',
         type=int,
-        default=500,
-        help="Specify width of single windows (default: %(default)s)")
+        default=62,
+        help="Specify expansion around breakpoints (default: %(default)s). This will automatically extend 8x the specified value for discordant reads")
     p.add_argument(
         '--gap',
         type=int,
-        default=10,
+        default=8,
         help="Specify width of gap (default: %(default)s)")
     p.add_argument("--reference", help="reference fasta file", required=True)
 
@@ -203,16 +242,29 @@ def main(args=sys.argv[1:]):
         parent.mkdir(mode=0o774, exist_ok=True)
     od.mkdir(mode=0o774, exist_ok=True)
 
-
     depths_by_chrom = zarr.open_group(os.path.join(a.directory, "depths.zarr.zip"))
 
+    # we need positions sorted by a and by b so we do that here.
     e2d_a = pd.read_table(f"{a.directory}/sv-channels.events2d.txt.gz", compression="gzip",
                         usecols=list(range(5)),
                         names=["a_chrom", "a_pos", "b_chrom", "b_pos", "event"],
-                        dtype={"a_chrom": str, "b_chrom": str})
+                        dtype={"a_chrom": str, "b_chrom": str, "a_pos": np.int32, "b_pos": np.int32})
+
+    edisc_a = e2d_a.iloc[e2d_a.event.isin(DISCORDANTS).values].copy(deep=True)
+    # we compress discordant position by 8
+    edisc_a.a_pos = edisc_a.a_pos.values >> 3
+    edisc_a.b_pos = edisc_a.b_pos.values >> 3
+
+    e2d_a = e2d_a.iloc[~e2d_a.event.isin(DISCORDANTS).values]
+
     e2d_a.sort_values("a_pos", inplace=True)
+    edisc_a.sort_values("a_pos", inplace=True)
+
     e2d_b = e2d_a.copy(deep=True)
     e2d_b.sort_values("b_pos", inplace=True)
+
+    edisc_b = edisc_a.copy(deep=True)
+    edisc_b.sort_values("b_pos", inplace=True)
 
     fasta = Fasta(a.reference, as_raw=True)
 
@@ -236,20 +288,21 @@ def main(args=sys.argv[1:]):
         toks = line.strip().split()
         # if toks[6] != 'DEL': continue
         if toks[0] != toks[3]: continue
-        if int(toks[1]) < expand: continue
-        if int(toks[4]) < expand: continue
-        if int(toks[4]) < int(toks[1]):
-            toks[4], toks[1] = toks[1], toks[4]
+        l, r = int(toks[1]), int(toks[4])
+        if l < expand: continue
+        if r < expand: continue
+        if r < l:
+            l, r = r, l
 
         clen = len(depths_by_chrom[toks[0]])
-        if int(toks[4]) >= clen - expand: continue
+        if r >= clen - expand: continue
         # here, sv_chan is shape (n_channels, 2 * expand + gap) can accumulate these and send to learner.
-        toks_id = str(int(toks[1])) + '_' + str(int(toks[4]))
+        toks_id = str(l) + '_' + str(r)
         if toks_id in dups_toks: continue
         dups_toks.add(toks_id)
         file_object.write(line)
 
-        events.append((toks[0], int(toks[1]), int(toks[4])))
+        events.append((toks[0], l, r))
 
     file_object.close()
 
@@ -266,6 +319,7 @@ def main(args=sys.argv[1:]):
     for i, event in enumerate(events):
 
         ch = generate_channels_for_event(event[0], event[1], event[2], e1d, e2d_a, e2d_b,
+                                             edisc_a, edisc_b,
                                              expand, gap, depths_by_chrom[event[0]], fasta)
         Z[i] = ch #.reshape((1, Z.shape[1], Z.shape[2]))
         if i % n_vars == 0 and i > 0:
@@ -273,53 +327,12 @@ def main(args=sys.argv[1:]):
             print(f"{i}/{Z.shape}. in {td:.0f} seconds ({n_vars/td:.1f} SVs/second \n{Z.info}", file=sys.stderr)
             sys.stderr.flush()
             t1 = time.time()
+        #plot_event(ch, toks, expand, gap)
 
     zarr.save_array(os.path.join(a.output, 'channels.zarr.zip'), Z)
     t = time.time() - t0
-    print(f"generated channels for {len(events)} SVs in {t:.1f} seconds ({len(events)/t:.0f} SVs/second)", file=sys.stderr)
+    print(f"[svchannels] generated channels for {len(events)} SVs in {t:.1f} seconds ({len(events)/t:.0f} SVs/second)", file=sys.stderr)
 
 
 if __name__ == "__main__":
-    import pandas as pd
-
-    if False:
-        outdir = "sv-channels"
-        chrom = "1"
-        depths = zarr.open(f"{outdir}/depths.{chrom}.bin", mode='r')
-
-        signals2d = np.array([
-            ("chr1", 11100, "chr3", 11400, Event(1)),
-            ("chr1", 11200, "chr1", 11300, Event(2)),
-            ("chr1", 11200, "chr1", 11400, Event(3)),
-            ("chr1", 11200, "chr2", 11400, Event(4)),
-            ("chr1", 11400, "chr1", 11200, Event(5)),
-            ("chr1", 11500, "chr1", 11900, Event(6))
-        ], dtype=[('a_chrom', 'S8'), ('a_pos', np.int32),
-                  ('b_chrom', 'S8'), ('b_pos', np.int32),
-                  ('event', 'i2')
-                  ])
-        signals2d = pd.DataFrame(signals2d)
-
-        signals1d = np.array([
-            ("chr1", 11100, Event(3)),
-            ("chr1", 11400, Event(3)),
-        ], dtype=[('chrom', 'S8'), ('pos', np.int32), ('event', 'i2')])
-        signals1d = pd.DataFrame(signals1d)
-
-        apos, bpos = (11100, 11400)
-        print(f"querying {apos}, {bpos}")
-
-        # print("all")
-        # print(signals2d.tolist())
-
-        expand = 100
-        gap = 10
-
-        # r = find_signals_for_event("chr1", 100, 400, signals2d, signals1d, expand=expand)
-        # for k, v in r.items():
-        #    print(k)
-        #    print(v.tolist())
-
-        print(generate_channels_for_event("chr1", apos, bpos, signals1d, signals2d, expand, gap, depths))
-
     main()
